d888640a73781e1b0e0c8b88b607888526cc95e5
AnasuaSamanta <73124055+AnasuaSamanta@users.noreply.github.com>
2021-01-25 18:57:25 +0530

    Add files via upload

---
+++ js/Player.js
@@ -0,0 +1,37 @@
+class Player {
+  constructor(){
+  this.index=null;
+  this.distance=0;
+  this.name=null; 
+   
+   
+  }
+
+  getCount(){
+    var playerCountRef = database.ref('playerCount');
+    playerCountRef.on("value",function(data){
+      playerCount = data.val();
+    })
+  }
+
+  updateCount(count){
+    database.ref('/').update({
+      playerCount: count
+    });
+  }
+
+  update(){
+    var playerIndex = "Players/player" +this.index;
+    database.ref(playerIndex).set({
+      name:this.name,
+      distance:this.distance
+    
+    });
+  }
+  getplayerinfo(){
+  var playerinfo = database.ref('Players');
+  playerinfo.on("value",function(data){
+    allplayers=data.val();
+  })
+  }
+}
